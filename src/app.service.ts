5b81af4ceb23e1ba4c6b52645f9916ee177640b7
RenanTashiro <renantashiro@hotmail.com>
2023-06-21 17:39:51 -0300

    feat(): send to s3

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import * as path from 'path';
 import * as fs from 'fs';
 import * as ffmpeg from 'fluent-ffmpeg';
+import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class AppService {
+  private bucketName = process.env.AWS_BUCKET_NAME;
+
+  constructor(
+    private s3 = new AWS.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    }),
+  ) {}
+
   public async save(data: string[]): Promise<string> {
     const timestamp = new Date().getTime();
-    const fileNames = await this.convertImagesToVideo(
-      data,
-      `output_${timestamp}`,
-      timestamp,
-    );
-    await this.deleteTempFiles(fileNames);
-    return 'Hello World!';
+    const outputVideoName = `output_${timestamp}`;
+    let fileNames;
+    try {
+      fileNames = await this.convertImagesToVideo(
+        data,
+        outputVideoName,
+        timestamp,
+      );
+      const videoFilePath = `${outputVideoName}.mp4`;
+      const uploadedFileData = await this.uploadToS3(videoFilePath);
+      await this.deleteTempFiles(fileNames);
+      return uploadedFileData.Location;
+    } catch (error) {
+      console.error('Error:', error);
+      if (fileNames) {
+        await this.deleteTempFiles(fileNames);
+      }
+      throw error;
+    }
+  }
+
+  private async uploadToS3(
+    filePath: string,
+  ): Promise<AWS.S3.ManagedUpload.SendData> {
+    const fileStream = fs.createReadStream(filePath);
+
+    const uploadParams = {
+      Bucket: this.bucketName,
+      Key: `videos/${path.basename(filePath)}`,
+      Body: fileStream,
+    };
+
+    return this.s3.upload(uploadParams).promise();
   }
 
   private async convertImagesToVideo(
@@ -33,13 +71,15 @@ export class AppService {
         .inputFPS(25)
         .outputOptions(
           '-vf',
-          'scale=640:-1',
+          'scale=720:720', // set output resolution
           '-c:v',
           'libx264',
+          '-crf',
+          '0', // set constant rate factor to 0 (lossless)
           '-pix_fmt',
           'yuv420p',
           '-r',
-          '30',
+          '30', // set output frames per second
         )
         .output(`${outputVideoName}.mp4`) // Replace with your output path
         .run();
@@ -59,7 +99,7 @@ export class AppService {
         `/tmp/${filename}`,
         base64Images[i],
         'base64',
-      ); // Replace with your images path
+      );
       filenames.push(`/tmp/${filename}`);
     }
 
