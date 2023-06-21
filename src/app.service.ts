b05859017959b883c237d03df278321a6b18ac79
RenanTashiro <renantashiro@hotmail.com>
2023-06-21 17:33:04 -0300

    feat(): delete created files

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,12 @@ import * as ffmpeg from 'fluent-ffmpeg';
 export class AppService {
   public async save(data: string[]): Promise<string> {
     const timestamp = new Date().getTime();
-    await this.convertImagesToVideo(data, `output_${timestamp}`, timestamp);
+    const fileNames = await this.convertImagesToVideo(
+      data,
+      `output_${timestamp}`,
+      timestamp,
+    );
+    await this.deleteTempFiles(fileNames);
     return 'Hello World!';
   }
 
@@ -14,14 +19,14 @@ export class AppService {
     base64Images: string[],
     outputVideoName: string,
     timestamp: number,
-  ) {
+  ): Promise<string[]> {
     // decode Base64 strings to image files
-    await this.decodeBase64Images(base64Images, timestamp);
+    const filenames = await this.decodeBase64Images(base64Images, timestamp);
 
     return new Promise((resolve, reject) => {
       ffmpeg()
         .on('end', () => {
-          resolve('Video created');
+          resolve(filenames);
         })
         .on('error', (err) => reject(new Error(`Error ${err}`)))
         .input(`/tmp/image_${timestamp}_%d.jpg`) // Replace with your images path
@@ -55,9 +60,20 @@ export class AppService {
         base64Images[i],
         'base64',
       ); // Replace with your images path
-      filenames.push(filename);
+      filenames.push(`/tmp/${filename}`);
     }
 
     return filenames;
+  }
+
+  private async deleteTempFiles(filenames: string[]): Promise<void> {
+    for (const filename of filenames) {
+      try {
+        await fs.promises.unlink(filename);
+        console.log(`Deleted file: ${filename}`);
+      } catch (err) {
+        console.error(`Error deleting file ${filename}: ${err}`);
+      }
+    }
   }
 }
