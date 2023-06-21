b2b68c3a0b27756ddddcfa9d0d4eebbebafacaa0
RenanTashiro <renantashiro@hotmail.com>
2023-06-21 17:23:38 -0300

    feat(): increase payload max size

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.use(bodyParser.json({ limit: '10mb' }));
-  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+  app.use(bodyParser.json({ limit: '100mb' }));
+  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
   await app.listen(4000);
 }
