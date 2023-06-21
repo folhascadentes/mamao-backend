54c3dc1e13650b3d4a615c7f30d3e2b5cb6b3221
RenanTashiro <renantashiro@hotmail.com>
2023-06-21 16:51:29 -0300

    feat(): enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(bodyParser.json({ limit: '5mb' }));
-  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+  app.enableCors();
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
   await app.listen(4000);
 }
