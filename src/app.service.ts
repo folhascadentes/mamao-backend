b706cb53361fb1d0c7b3f77a78ae61842bbbd6b9
Renan Tashiro <renantashiro@hotmail.com>
2023-06-24 20:15:47 -0300

    fix(): upload to s3 using new aws-sdk version

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,24 +2,18 @@ import { Injectable } from '@nestjs/common';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as ffmpeg from 'fluent-ffmpeg';
-import * as AWS from 'aws-sdk';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class AppService {
   private bucketName = process.env.AWS_BUCKET_NAME;
-
-  constructor(
-    private s3 = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION,
-    }),
-  ) {}
+  private s3 = new S3Client({ region: 'sa-east-1' });
 
   public async save(data: string[]): Promise<string> {
     const timestamp = new Date().getTime();
     const outputVideoName = `output_${timestamp}`;
     let fileNames;
+
     try {
       fileNames = await this.convertImagesToVideo(
         data,
@@ -28,10 +22,9 @@ export class AppService {
       );
       const videoFilePath = `${outputVideoName}.mp4`;
       const uploadedFileData = await this.uploadToS3(videoFilePath);
-      await this.deleteTempFiles(fileNames);
-      return uploadedFileData.Location;
+      await this.deleteTempFiles(fileNames.concat(videoFilePath));
+      return uploadedFileData.ETag;
     } catch (error) {
-      console.error('Error:', error);
       if (fileNames) {
         await this.deleteTempFiles(fileNames);
       }
@@ -39,18 +32,18 @@ export class AppService {
     }
   }
 
-  private async uploadToS3(
-    filePath: string,
-  ): Promise<AWS.S3.ManagedUpload.SendData> {
+  private async uploadToS3(filePath: string) {
+    console.log('UPLOADING TO S3', filePath);
+
     const fileStream = fs.createReadStream(filePath);
 
-    const uploadParams = {
+    const command = new PutObjectCommand({
       Bucket: this.bucketName,
       Key: `videos/${path.basename(filePath)}`,
       Body: fileStream,
-    };
+    });
 
-    return this.s3.upload(uploadParams).promise();
+    return await this.s3.send(command);
   }
 
   private async convertImagesToVideo(
@@ -70,16 +63,12 @@ export class AppService {
         .input(`/tmp/image_${timestamp}_%d.jpg`) // Replace with your images path
         .inputFPS(25)
         .outputOptions(
-          '-vf',
-          'scale=720:720', // set output resolution
           '-c:v',
-          'libx264',
+          'libx265',
           '-crf',
-          '0', // set constant rate factor to 0 (lossless)
-          '-pix_fmt',
-          'yuv420p',
+          '28',
           '-r',
-          '30', // set output frames per second
+          '30',
         )
         .output(`${outputVideoName}.mp4`) // Replace with your output path
         .run();
