1d0570d90f76442a43105200f26e291bf123333c
Renan Tashiro <renantashiro@hotmail.com>
2023-06-25 11:00:24 -0300

    fix(): remove outdated tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseService } from './database.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +8,11 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [DatabaseService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
-  });
+  describe('root', () => {});
 });
