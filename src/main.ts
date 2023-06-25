ddf328585ac79808153141cbdd3b3fdb510d579b
Renan Tashiro <renantashiro@hotmail.com>
2023-06-25 20:29:36 -0300

    feat(): check if env variables is defined

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,35 @@ import { NestFactory } from '@nestjs/core';
 import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+
+const envVariables = ['AWS_BUCKET_NAME'];
+
+async function checkEnvVariables() {
+  for (let envVariable of envVariables) {
+    if (!process.env[envVariable]) {
+      throw new Error(`Environment variable ${envVariable} is missing`);
+    }
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    const credentialsFilePath = path.join(os.homedir(), '.aws', 'credentials');
+
+    return new Promise((resolve) => {
+      fs.access(credentialsFilePath, fs.constants.F_OK, (err) => {
+        if (err) {
+          throw new Error(
+            'Please create AWS credentials file (~/.aws/credentials))',
+          );
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,4 +42,10 @@ async function bootstrap() {
 
   await app.listen(4000);
 }
-bootstrap();
+
+async function main() {
+  await checkEnvVariables();
+  await bootstrap();
+}
+
+main();
