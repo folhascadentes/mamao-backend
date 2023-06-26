cf935ff4c60bbd142c7ca65fc844e55ac65ad633
Renan Tashiro <renantashiro@hotmail.com>
2023-06-26 20:53:56 -0300

    refactor(): extract filterUserAttributes

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -1,69 +1,42 @@
-// Import dependencies
 import { Injectable } from '@nestjs/common';
 import {
+  AttributeType,
   CognitoIdentityProviderClient,
   ConfirmForgotPasswordCommand,
-  ConfirmForgotPasswordCommandOutput,
   ConfirmSignUpCommand,
-  ConfirmSignUpCommandOutput,
   DeleteUserCommand,
   DeleteUserCommandOutput,
   ForgotPasswordCommand,
-  ForgotPasswordCommandOutput,
   InitiateAuthCommand,
-  InitiateAuthCommandOutput,
   SignUpCommand,
-  SignUpCommandOutput,
   UpdateUserAttributesCommand,
-  UpdateUserAttributesCommandOutput,
 } from '@aws-sdk/client-cognito-identity-provider';
 import * as crypto from 'crypto';
 import { SignUpPayload, UpdateProfilePayload } from './types';
 
 @Injectable()
 export class AuthService {
-  private cognito = new CognitoIdentityProviderClient({
-    region: process.env.AWS_REGION,
-  });
+  private readonly cognito: CognitoIdentityProviderClient;
 
-  public async signUp(payload: SignUpPayload): Promise<SignUpCommandOutput> {
+  constructor() {
+    this.cognito = new CognitoIdentityProviderClient({
+      region: process.env.AWS_REGION,
+    });
+  }
+
+  public async signUp(payload: SignUpPayload) {
     const command = new SignUpCommand({
       ClientId: process.env.COGNITO_CLIENT_ID,
       Username: payload.email,
       Password: payload.password,
       SecretHash: this.calculateHMAC(payload.email),
-      UserAttributes: Object.keys(payload)
-        .map((key) => {
-          if (key === 'email' || key === 'password') {
-            return null;
-          }
-
-          const name = `custom:${key}`;
-          let value = payload[key];
-
-          if (typeof value === 'number') {
-            value = value.toString();
-          }
-
-          if (value) {
-            return {
-              Name: name,
-              Value: value,
-            };
-          }
-
-          return null;
-        })
-        .filter(Boolean), // remove null entries
+      UserAttributes: this.filterUserAttributes(payload),
     });
 
     return await this.cognito.send(command);
   }
 
-  public async confirmSignUp(
-    email: string,
-    code: string,
-  ): Promise<ConfirmSignUpCommandOutput> {
+  public async confirmSignUp(email: string, code: string) {
     const command = new ConfirmSignUpCommand({
       ClientId: process.env.COGNITO_CLIENT_ID,
       Username: email,
@@ -74,10 +47,7 @@ export class AuthService {
     return await this.cognito.send(command);
   }
 
-  public async signIn(
-    email: string,
-    password: string,
-  ): Promise<InitiateAuthCommandOutput> {
+  public async signIn(email: string, password: string) {
     const command = new InitiateAuthCommand({
       AuthFlow: 'USER_PASSWORD_AUTH',
       ClientId: process.env.COGNITO_CLIENT_ID,
@@ -91,9 +61,7 @@ export class AuthService {
     return await this.cognito.send(command);
   }
 
-  public async forgetPassword(
-    email: string,
-  ): Promise<ForgotPasswordCommandOutput> {
+  public async forgetPassword(email: string) {
     const command = new ForgotPasswordCommand({
       ClientId: process.env.COGNITO_CLIENT_ID,
       Username: email,
@@ -107,7 +75,7 @@ export class AuthService {
     email: string,
     code: string,
     newPassword: string,
-  ): Promise<ConfirmForgotPasswordCommandOutput> {
+  ) {
     const command = new ConfirmForgotPasswordCommand({
       ClientId: process.env.COGNITO_CLIENT_ID,
       Username: email,
@@ -122,32 +90,10 @@ export class AuthService {
   public async updateProfile(
     payload: UpdateProfilePayload,
     accessToken: string,
-  ): Promise<UpdateUserAttributesCommandOutput> {
+  ) {
     const command = new UpdateUserAttributesCommand({
       AccessToken: accessToken,
-      UserAttributes: Object.keys(payload)
-        .map((key) => {
-          if (key === 'accessToken') {
-            return null;
-          }
-
-          const name = `custom:${key}`;
-          let value = payload[key];
-
-          if (typeof value === 'number') {
-            value = value.toString();
-          }
-
-          if (value) {
-            return {
-              Name: name,
-              Value: value,
-            };
-          }
-
-          return null;
-        })
-        .filter(Boolean),
+      UserAttributes: this.filterUserAttributes(payload),
     });
 
     return await this.cognito.send(command);
@@ -168,5 +114,14 @@ export class AuthService {
     const hmac = crypto.createHmac('sha256', process.env.COGNITO_SECRET_KEY);
     hmac.update(message);
     return hmac.digest('base64');
+  }
+
+  private filterUserAttributes(payload: any): AttributeType[] {
+    return Object.entries(payload)
+      .filter(([key, value]) => key !== 'accessToken' && value)
+      .map(([key, value]) => ({
+        Name: `custom:${key}`,
+        Value: typeof value === 'number' ? value.toString() : value,
+      })) as AttributeType[];
   }
 }
