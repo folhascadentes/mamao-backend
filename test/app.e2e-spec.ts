42481fd54c6c8be7b6150c10275b1edf17e6636a
Renan Tashiro <renantashiro@hotmail.com>
2023-06-26 20:57:48 -0300

    tests(): add tests to auth flow

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let server: any;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,5 +14,104 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    server = app.getHttpServer();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('should sign up successfully', async () => {
+    const payload = {
+      email: 'test@example.com',
+      password: 'testpassword',
+    };
+
+    const response = await request(server)
+      .post('/sign-up')
+      .send(payload)
+      .expect(201);
+
+    expect(response.body).toBeDefined();
+  });
+
+  it('should confirm sign up successfully', async () => {
+    const payload = {
+      email: 'test@example.com',
+      code: '123456',
+    };
+
+    const response = await request(server)
+      .post('/confirm-sign-up')
+      .send(payload)
+      .expect(200);
+
+    expect(response.body).toBeDefined();
+  });
+
+  it('should sign in successfully', async () => {
+    const payload = {
+      email: 'test@example.com',
+      password: 'testpassword',
+    };
+
+    const response = await request(server)
+      .post('/sign-in')
+      .send(payload)
+      .expect(200);
+
+    expect(response.body).toBeDefined();
+  });
+
+  it('should forget password successfully', async () => {
+    const payload = {
+      email: 'test@example.com',
+    };
+
+    const response = await request(server)
+      .post('/forget-password')
+      .send(payload)
+      .expect(200);
+
+    expect(response.body).toBeDefined();
+  });
+
+  it('should confirm forget password successfully', async () => {
+    const payload = {
+      email: 'test@example.com',
+      code: '123456',
+      newPassword: 'newpassword',
+    };
+
+    const response = await request(server)
+      .post('/confirm-forget-password')
+      .send(payload)
+      .expect(200);
+
+    expect(response.body).toBeDefined();
+  });
+
+  it('should update profile successfully', async () => {
+    const payload = {
+      age: 25,
+      gender: 'female',
+    };
+
+    const response = await request(server)
+      .post('/update-profile')
+      .send(payload)
+      .set('Authorization', 'Bearer {token}')
+      .expect(200);
+
+    expect(response.body).toBeDefined();
+  });
+
+  it('should delete account successfully', async () => {
+    const response = await request(server)
+      .delete('/delete-account')
+      .set('Authorization', 'Bearer {token}')
+      .expect(200);
+
+    expect(response.body).toBeDefined();
   });
 });
